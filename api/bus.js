5175f831f0d31eef4f96da97158f1333f8c5ad64
gearchain66 <harri.gibson@yahoo.com>
2026-05-09 20:14:59 +1200

    Add files via upload

--- api/bus.js
+++ api/bus.js
@@ -2,7 +2,6 @@ module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
-
   const stopId = process.env.METLINK_STOP_ID;
   const apiKey = process.env.METLINK_API_KEY;
   const label  = process.env.METLINK_STOP_LABEL || ('Stop ' + stopId);
